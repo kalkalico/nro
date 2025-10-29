108120a0da4abe14eb18dad439b6b2c5628253ac
kal <114609717+kalkalico@users.noreply.github.com>
2025-10-29 19:56:13 -0400

    Update bypass.cmd

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,13 @@
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/kalkalico/nro/refs/heads/main/unattend.xml
-%WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
+@echo off
+
+curl -L -o "C:\Windows\Panther\unattend.xml" "https://raw.githubusercontent.com/kalkalico/nro/refs/heads/main/unattend.xml"
+IF ERRORLEVEL 1 (
+    echo Failed to download unattend.xml
+    exit /b 1
+)
+
+"%WINDIR%\System32\Sysprep\Sysprep.exe" /oobe /unattend:"C:\Windows\Panther\unattend.xml" /reboot
+IF ERRORLEVEL 1 (
+    echo Sysprep encountered an error
+    exit /b 1
+)
